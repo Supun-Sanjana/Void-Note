5567c628c67ab79efcaca8c05e06e1f1b0147432
Supun Sanjana <infor.ssupun@gmail.com>
2025-08-03 00:56:32 +0530

    create navigation across all directory

--- apps/app/app/(appCon)/dashboard/page.tsx
+++ apps/app/app/(appCon)/dashboard/page.tsx
@@ -5,14 +5,7 @@ const page = () => {
     return (
         <div className='bg-black mx-10'>
 
-            <div className='w-full h-[170px]  bg-[#141414] px-[30px] rounded-2xl border-1 border-[#3A3D47]'>
-                <div className='flex gap-2 py-[40px] px-[30px]'>
-                    <input placeholder='Add a new task ...' type="text" className='pl-[20px] w-full border-[1px] rounded-[5px] mr-3 border-[#3A3D47]' />
-                    <button className='flex gap-2 bg-[#676BEB] px-6 py-1 rounded-[5px]'><Plus /> Add   </button>
-                </div>
-                <p className='flex justify-center text-[#717171]'>Press enter to quickly add , or click ‘Add Task’ </p>
 
-            </div>
 
             <div className="felx-2 flex gap-10 mt-10 ">
                 <div className="p-4 left-panel flex-1 h-full rounded-[10px] bg-[#141414] border border-[#3A3D47]">
