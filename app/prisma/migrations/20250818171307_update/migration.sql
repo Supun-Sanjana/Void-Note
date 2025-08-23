65c05f5d2c20f92c2d49a78da74319cacff09985
Supun Sanjana <infor.ssupun@gmail.com>
2025-08-23 16:33:53 +0530

    update prisma schema for supabase db

--- app/prisma/migrations/20250818171307_update/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Task" ALTER COLUMN "Details" DROP NOT NULL,
-ALTER COLUMN "Priority" SET DEFAULT 'Low',
-ALTER COLUMN "Status" SET DEFAULT 'Pending',
-ALTER COLUMN "Due_Date" DROP NOT NULL;
